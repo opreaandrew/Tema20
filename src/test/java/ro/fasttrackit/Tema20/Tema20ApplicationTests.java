6440f6854d92371702b1abcc4dda12929c5dedf4
Andrei Oprea <opreaandrew@yahoo.com>
2023-11-17 05:48:46 +0200

    Started

---
+++ src/test/java/ro/fasttrackit/Tema20/Tema20ApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.fasttrackit.Tema20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tema20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
